f7aac979ac9cd2d2ffa9b4f5fbe0c6df31026d17
Ajeesh Joshy <jishcem@gmail.com>
2022-08-28 11:45:36 +0530

    Fix the start server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 echo "Starting the server"
 
 sudo npm install --prefix /home/ubuntu/Code/express-codedeploy-2
-cd /home/ubuntu/Code/express-codedeploy-2
-npm run build
-sudo systemctl start node-api.service
+npm run build --prefix /home/ubuntu/Code/express-codedeploy-2
+sudo systemctl restart node-api.service
